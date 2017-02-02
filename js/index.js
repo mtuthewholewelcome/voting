316787e037360a783a417ec59c746ff248736515
lynnphayu <zawlinnnaing0018@gmail.com>
2017-02-03 01:16:44 +0630

    design reconfig

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$( document ).ready( function( ) {
+
       var config = {
             apiKey: "AIzaSyALn_c79BvSqEQAbNKGBNYYXVJCDEWER-Y",
             authDomain: "voting-d2df5.firebaseapp.com",
@@ -6,50 +7,136 @@ $(document).ready(function(){
             storageBucket: "voting-d2df5.appspot.com",
             messagingSenderId: "1032494254100"
       };
-      firebase.initializeApp(config);
-      
-      $('.container').hide();
+      firebase.initializeApp( config );
 
-      $('.king_vote').click( function(e){ 
-            e.preventDefault();
-            var ref = firebase.database().ref().child('numberone/vote/king');
-            ref.transaction(function(vote) {
-                  if(vote != null){
+      var king_vote = firebase.database( ).ref( ).child( 'king_vote' );
+      var queen_vote = firebase.database( ).ref( ).child( 'queen_vote' );
+      var king_voted_list = firebase.database( ).ref( ).child( 'king' );
+      var queen_voted_list = firebase.database( ).ref( ).child( 'queen' );
+
+      $('#float').hide();
+
+      $( '.king' ).click( function( e ) {
+            e.preventDefault( );
+
+            king_vote.child( e.target.id ).transaction( function( vote ) {
+                  if ( vote != null ) {
                         vote++;
+                        var updates = {};
+                        updates[ 'king/' + firebase.auth( ).currentUser.uid ] = 1;
+                        firebase.database( ).ref( ).update( updates );
+                        $( '.king' ).prop( 'disabled', true );
                   }
                   return vote;
-            });
-      });
+            } );
+      } );
 
-      $('button').click( function(e){
-            $('#facebook').hide();
-                  $('.container').show();
-            e.preventDefault();
-            var provider = new firebase.auth.FacebookAuthProvider();
-            firebase.auth().signInWithPopup(provider)
-            .then( function(result) {
-                  
-            })
-            .catch(function(error) {
-                  var errorCode = error.code;
-                  var errorMessage = error.message;
-                  var email = error.email;
-                  var credential = error.credential;
-                  console.log(errorMessage);
+      $( '.queen' ).click( function( e ) {
+            e.preventDefault( );
 
-            });
-
-            
-      });
-
-      firebase.auth().onAuthStateChanged(function(user) {
-                  if (user) {
-                        console.log(user);
-                  } else {
-                        console.log("No User!")
+            queen_vote.child( e.target.id ).transaction( function( vote ) {
+                  if ( vote != null ) {
+                        vote++;
+                        var updates = {};
+                        updates[ 'queen/' + firebase.auth( ).currentUser.uid ] = 1;
+                        firebase.database( ).ref( ).update( updates );
+                        $( '.queen' ).prop( 'disabled', true );
                   }
+                  return vote;
+            } );
+      } );
 
-      });
 
-});
+
+
+      $( '#facebook_login' ).click( function( e ) {
+
+            e.preventDefault( );
+            var provider = new firebase.auth.FacebookAuthProvider( );
+            firebase.auth( ).signInWithPopup( provider )
+                  .then( function( result ) {
+                        
+                        // $( '#facebook_login' ).hide( );
+                        // $( '#float' ).show();
+
+                        king_voted_list.orderByKey( )
+                              .equalTo( result.user.uid )
+                              .once( 'value' )
+                              .then( function( snapshot ) {
+                                    var value = snapshot.val( );
+                                    if ( value ) {
+                                          $( '.king' ).prop( 'disabled', true );
+                                    }
+                              } );
+
+                        queen_voted_list.orderByKey( )
+                              .equalTo( result.user.uid )
+                              .once( 'value' )
+                              .then( function( snapshot ) {
+                                    var value = snapshot.val( );
+                                    if ( value ) {
+                                          $( '.queen' ).prop( 'disabled', true );
+                                    }
+                              } );
+
+                  } )
+                  .catch( function( error ) {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        var email = error.email;
+                        var credential = error.credential;
+                        console.log( errorMessage );
+
+                  } );
+
+
+      } );
+
+      firebase.auth( ).onAuthStateChanged( function( user ) {
+            if ( user ) {
+                  
+                  $( '#facebook_login' ).hide( );
+                  $( '#float' ).show( );
       
+      
+                  console.log( firebase.auth( ).currentUser.uid );
+                  king_voted_list.orderByKey( )
+                        .equalTo( user.uid )
+                        .once( 'value' )
+                        .then( function( snapshot ) {
+                              var value = snapshot.val( );
+                              if ( value ) {
+                                    $( '.king' ).prop( 'disabled', true );
+                              }
+                        } );
+
+                  queen_voted_list.orderByKey( )
+                        .equalTo( user.uid )
+                        .once( 'value' )
+                        .then( function( snapshot ) {
+                              var value = snapshot.val( );
+                              if ( value ) {
+                                    $( '.queen' ).prop( 'disabled', true );
+                              }
+                        } );
+
+
+            } else {
+                  console.log( "No User!" )
+            }
+
+      } );
+
+      $( "#king" ).click( function( ) {
+            $( "#panel" ).animate( {
+                  left: "-100%"
+            }, 1000 );
+
+      } );
+
+      $( "#queen" ).click( function( ) {
+            $( "#panel" ).animate( {
+                  left: "0"
+            }, 1000 );
+      } );
+} );
